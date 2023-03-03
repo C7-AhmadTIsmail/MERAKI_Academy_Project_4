7d21d501a1045f5aafa0ab1a01f2d729c0a20de5
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-03-03 18:25:29 +0300

    0. start & db initialy (back end)

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const db=require("./models/db")
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// console.log(process.env,"env") to test div run
+
+app.use(cors());
+app.use(express.json());
+
+// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
