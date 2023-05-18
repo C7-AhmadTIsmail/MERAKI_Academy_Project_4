bb36898f354c5525f62aca5dd0ae676c00bc669a
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-05-19 01:47:35 +0300

    edite_1

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
-const URL=process.env.URL_DB || "mongodb://127.0.0.1:27017/Project4"
+const URL=process.env.URL_DB
 
 mongoose.connect(`${URL}`
 ).then(() => {
